e98cd4421b60765f2c206242d1776bcd3935a9da
codenextitD <engr.ismail348@gmail.com>
2023-02-26 23:59:40 +0600

    frontend setup

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 // Admin All Route
 Route::controller(\App\Http\Controllers\AdminController::class)->group(function () {
